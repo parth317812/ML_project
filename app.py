e2e22b070b4fd195c312d5e18be95cb68a09bcfd
parth chauhan <cparth317@gmail.com>
2024-05-30 17:40:33 +0530

    some edit

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def predict():
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
